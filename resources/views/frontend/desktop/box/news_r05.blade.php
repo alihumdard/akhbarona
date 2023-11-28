89c95cbf94b7d58f0f326b3279bc13e35875d883
Usama <usamabadarbasra@gmail.com>
2023-11-28 14:19:24 +0500

    رياضة

--- resources/views/frontend/desktop/box/news_r05.blade.php
+++ resources/views/frontend/desktop/box/news_r05.blade.php
@@ -15,37 +15,34 @@
                 </div>
             </div>
             <div class="col-md-8 mt-3">
+                <div class="row">
                 <?php foreach($newsR05 as $index=>$article) { ?>
-            @if($index < 4)
-            <div class="col-md-6 mt-3">
-                <div class="main-box">
-                    <div>
-                        <a href="{{Common::article_link($article)}}">
-                            <img class="img-fluid" src="{{$fileRepo->getLarge($article->image,true,$article->md5_file)}}" alt="{{$article->image_caption?$article->image_caption:$article->title}}" /><br />
-                        </a>
-                    </div>
-                    <div class="main-box-text">
-                        <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
-                    </div>
-                </div>
-            </div>
-            @else
-            <div class="col-md-12 mt-3">
-                <div class="d-flex"
-                    style="border: 0.5px solid #D0D0D0;">
-                    <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
-                    <div>
-                        <div class="image">
+                    @if($index < 4)
+                    <div class="col-md-6 mt-3">
+                        <div class="main-box">
                             <a href="{{Common::article_link($article)}}">
-                                <img class="img-fluid h-100" src="{{$fileRepo->getLarge($article->image,true,$article->md5_file)}}"alt="{{$article->image_caption?$article->image_caption:$article->title}}" /><br />
+                                <img class="img-fluid" src="{{$fileRepo->getLarge($article->image,true,$article->md5_file)}}" alt="{{$article->image_caption?$article->image_caption:$article->title}}" />
                             </a>
+                            <div class="main-box-text">
+                                <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
+                            </div>
                         </div>
                     </div>
-
+                @else
+           <div class="col-md-12 mt-3">
+                    <div class="d-flex" style="border: 0.5px solid #D0D0D0;">
+                        <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
+                        <div class="image">
+                            <a href="{{Common::article_link($article)}}">
+                                <img class="img-fluid h-100" src="{{ $article->image ? $fileRepo->getLarge($article->image,true,$article->md5_file) : 'admin/assets/img/no-image.png' }}"  alt="{{$article->image_caption?$article->image_caption:$article->title}}" />
+                            </a>
+                            
+                        </div>
+                    </div>
                 </div>
-            </div>
             @endif
             <?php } ?>
 
+        </div>
         </div>
 @endif
