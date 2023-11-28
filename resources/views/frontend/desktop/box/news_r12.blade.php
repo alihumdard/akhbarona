0dc1a91a52d314bea7bc3b0cb3298b0f6695b2d3
Usama <usamabadarbasra@gmail.com>
2023-11-28 15:08:51 +0500

    الأخيرة

--- resources/views/frontend/desktop/box/news_r12.blade.php
+++ resources/views/frontend/desktop/box/news_r12.blade.php
@@ -1,53 +1,52 @@
 @if(count($newsR12n) > 0)
-    <div class="box1 box_white1">
-    <div id="category_news_box" >
-        <div class="main_news_box_row">
-        <a title="المزيد في الأخيرة" href="{{Common::mobileLink()}}last"><h3 class="box_title_two title_two">الأخيرة</h3></a>
-        </div>
-        <div id="latest_news_two" class="box_two box_white_two">
-            <ul>
-                <?php $article = $newsR12n[0];unset($newsR12n[0])?>
-                    <div class="short_two_one">
-                        <div class="short_holder_two_one">
-                            @if($article->image)
-                                <div class="image">
-                                    <a href="{{Common::article_link($article)}}">
-                                        <img src="{{$fileRepo->getMedium($article->image,true,$article->md5_file)}}" width="250" height="200" alt="{{$article->image_caption?$article->image_caption:$article->title}}" /><br />
-                                    </a>
-                                </div>
-                            @endif
-                            <div class="clearer"> </div>
-                            <h2 class="article_title"><a href="{{Common::article_link($article)}}">{{$article->title}}</a></h2>
-                            <p>
-                                {{$article->abstract?$article->abstract:Common::subWords($article->body,25)}} @if($article->body)...@endif
-                                @if(!$article->link)
-                                    @if($article->body)
-                                        <a href="{{Common::article_link($article)}}"> {{Config::get("site.lang.LNG_FULL_STORY")}}</a>
-                                    @endif
-                                @else
-                                    <a class="visit" href="{{$article->link}}"><img src="{{$fileRepo->getDesktopUrl("img/external.png")}}" alt="{{Config::get("site.lang.LNG_VISIT_WEBSITE")}}"/></a>
-                                @endif
-                            </p>
-                        </div>
-                    </div>
+
+
+<div class="heading mt-4">
+
+    <a title="المزيد في الأخيرة" href="{{Common::mobileLink()}}last"> <span>الأخيرة</span></a><svg xmlns="http://www.w3.org/2000/svg" width="10" height="47"
+    viewBox="0 0 10 50" style="margin-bottom: 8px; margin-left: -5px;" fill="none">
+    <rect y="25" width="10" height="25" fill="#2E4866" />
+    <rect width="10" height="25" fill="#C2111E" />
+</svg>
+</div>
+<!-- horizantal line -->
+<hr class="red-line">
+<?php $article = $newsR12n[0];unset($newsR12n[0])?>
+<div>
+    <div class="row">
+        <div class="col-12 col-md-12 col-lg-6">
+            <div class="row">
                 @foreach($newsR12n as $article)
-                    <div class="short_two">
-                        <div class="short_holder_two">
-                            @if($article->image)
-                                <div class="image">
-                                    <a href="{{Common::article_link($article)}}">
-                                        <img src="{{$fileRepo->getSummaryLarge($article->image,true,$article->md5_file)}}" width="161" height="110" alt="{{$article->image_caption?$article->image_caption:$article->title}}" /><br />
-                                    </a>
-                                </div>
-                            @endif
-                        <div class="clearer"> </div>
-                            <h2 class="article_title_two"><a href="{{Common::article_link($article)}}">{{$article->title}}</a></h2>
+                <div class="col-md-6 mt-3">
+                 
+                    <div>
+                    <a href="{{Common::article_link($article)}}">
+                        <img class="img-fluid" src="{{$fileRepo->getSummaryLarge($article->image,true,$article->md5_file)}}" alt="{{$article->image_caption?$article->image_caption:$article->title}}">
+                    </a>
+                    </div>
+                        <div class="main-box-text">
+                            <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
                         </div>
                     </div>
-                @endforeach
-                <div class="clearer"> </div>
-            </ul>
+                    @endforeach
+                </div>
+            </div>
+        
+
+
+        <div class="col-12 col-md-12 col-lg-6 mt-3">
+            <div class="news-6-last-img">
+                @if($article->image)
+                <div>
+                    <a href="{{Common::article_link($article)}}">
+                <img class="img-fluid"src="{{$fileRepo->getMedium($article->image,true,$article->md5_file)}}" alt="{{$article->image_caption?$article->image_caption:$article->title}}">
+            </a>
+            </div>
+            @endif
+                <p><a href="{{Common::article_link($article)}}">{{$article->title}}</a></p>
+            </div>
         </div>
+
     </div>
 </div>
 @endif
